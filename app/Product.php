01c0ef082a4bc77ff3bd2efdcf3260c5725fdf7a
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-06-27 15:21:43 +0600

    7 properties for product-s8_t2

--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,28 @@
 
 namespace App;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVAILABLE_PRODUCT = 'available' ;
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+
+    ];
+
+    public function isAvailable()
+    {
+
+        return $this->status == Product::AVAILABLE_PRODUCT;
+    }
+
 }

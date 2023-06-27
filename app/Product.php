e4660b4d04fc9a2d0cabfd9eec0074063902ecb6
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-06-27 17:47:14 +0600

    10 relationships between models-s8_t5

--- app/Product.php
+++ app/Product.php
@@ -3,6 +3,9 @@
 namespace App;
 
 
+use App\Seller;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
@@ -25,5 +28,21 @@ class Product extends Model
 
         return $this->status == Product::AVAILABLE_PRODUCT;
     }
+
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
 
 }

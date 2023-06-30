e985e63cfc7fe1abb89fd3cec5215fe06a7c17ca
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-06-30 10:00:46 +0600

    16 implementing the operations for buyers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -25,7 +25,8 @@ class DatabaseSeeder extends Seeder
         Transaction::truncate();
         DB::table('category_product')->truncate();
 
-        $usersQuantity = 200;
+       // $usersQuantity = 200;
+        $usersQuantity = 1000;
         $categoriesQuantity = 30;
         $productsQuantity = 1000;
         $transactionsQuantity = 1000;

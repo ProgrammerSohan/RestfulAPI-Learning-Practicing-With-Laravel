eb37903ce93e46725a8c5968eb66b7d26560e4a2
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-01 21:02:48 +0600

    30 handling unexpected exceptions_s15_t9

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -97,6 +97,11 @@ class Handler extends ExceptionHandler
         }
 
         return parent::render($request, $exception);
+
+        if(config('app.debug')){
+            return parent::render($request, $exception);
+        }
+        return $this->errorResponse('Unexpected Exception. Try later', 500);
     }
 
     protected function unauthenticated($request, AuthenticationException $exception)

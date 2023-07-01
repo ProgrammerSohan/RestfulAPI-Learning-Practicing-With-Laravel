de661e07e58696f6c8e625c028f1bb9988b5e239
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-01 15:15:37 +0600

    23 returning model not found errors as a json response_s15_t2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use App\Traits\ApiResponser;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -53,6 +54,13 @@ class Handler extends ExceptionHandler
     {
         if($exception instanceof ValidationException){
             return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+
+        if($exception instanceof ModelNotFoundException){
+            $modelName = strtolower(class_basename($exception->getModel()));
+
+            return $this->errorResponse("Does not exists any {$modelName} with the specified identificator", 404);
+
         }
 
         return parent::render($request, $exception);
